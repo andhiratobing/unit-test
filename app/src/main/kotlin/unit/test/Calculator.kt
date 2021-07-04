86c58438e035e36f692930166748c520f45f6f29
Andhi Ratobing <ratobing6@gmail.com>
2021-07-05 01:27:23 +0700

    Update

--- app/src/main/kotlin/unit/test/Calculator.kt
+++ app/src/main/kotlin/unit/test/Calculator.kt
@@ -2,7 +2,18 @@ package unit.test
 
 class Calculator {
 
+    /*Penjumlahan*/
     fun sum(first: Int, second: Int): Int {
         return first + second
     }
+
+    /*Pembagian dengan nilai 0 tidak boleh (IllegalArgumentException)*/
+    fun divide(first: Int, second: Int): Int {
+        if (second == 0){
+            throw IllegalArgumentException("Can't divide by zero")
+        }else{
+            return first/second
+        }
+    }
+
 }
--- app/src/test/kotlin/unit/test/CalculatorTest.kt
+++ app/src/test/kotlin/unit/test/CalculatorTest.kt
@@ -3,14 +3,96 @@ package unit.test
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.*
 import org.junit.jupiter.api.Assertions.*
+import org.opentest4j.TestAbortedException
+import org.junit.jupiter.api.Assumptions.*
+import unit.test.generator.SimpleDisplayNameGenerator
 
+/*Mengubah nama tes otomatis menggunakan @DisplayNameGeneration*/
+@DisplayNameGeneration(SimpleDisplayNameGenerator::class)
 class CalculatorTest {
 
-    val calculator = Calculator()
+    private val calculator = Calculator()
 
     @Test
+    @DisplayName ("Test for sum success")
     fun testSumSuccess(){
         val result = calculator.sum(20,5)
-        assertEquals(25, result)
+        assertEquals(25, result, "Hasilnya harusnya 25")
     }
+
+    @Test
+    @DisplayName("Test for divide success")
+    fun testDivideSuccess(){
+        val result = calculator.divide(10,2)
+        assertEquals(5, result)
+    }
+
+    @Test
+    /*Pembagian dengan nilai 0 tidak boleh (IllegalArgumentException)*/
+    @DisplayName("Test for divide by zero")
+    fun testDivideByZeroFailed(){
+        //Assertions Exceptions
+        assertThrows<IllegalArgumentException> {
+            calculator.divide(10, 0)
+        }
+    }
+
+    @Test
+    /*Jika ingin menonaktifkan unit tes tambahkan anotasi @Disabled*/
+    @Disabled("Sedang dikerjakan")
+    fun testComingSoon(){
+        // Belum selesai membuat unit tes
+    }
+
+    /*Menandai setiap function yang akan dieksekusi sebelum unit test dijalankan*/
+    @BeforeEach
+    fun beforeEach(){
+        println("Sebelum dieksekusi")
+    }
+
+    /*Menandai setiap function yang akan dieksekusi setelah unit test dijalankan*/
+    @AfterEach
+    fun afterEach(){
+        println("Setelah dieksekusi")
+    }
+
+    /*Membuat function static atau object function*/
+    companion object{
+        @BeforeAll
+        @JvmStatic
+        fun beforeAll(){
+            println("Sebelum semua dieksekusi")
+        }
+
+        @AfterAll
+        @JvmStatic
+        fun afterAll(){
+            println("Setelah semua dieksekusi")
+        }
+    }
+
+
+    /*Membatalkan unit test secara manual*/
+    @Test
+    fun testAborted(){
+        val profile = System.getenv()["PROFILE"]
+        if ("DEV" !=profile){
+            throw TestAbortedException()
+        }else {
+            println("Test not aborted")
+        }
+    }
+
+    /*Membatalkan unit test secara otomatis*/
+    @Test
+    fun testAssumptions(){
+        //Jika benar
+        assumeTrue("DEV" == System.getenv()["PROFILE"])
+
+        //Jika salah
+        println("Test not aborted")
+    }
+
+
+
 }
